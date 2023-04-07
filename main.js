bfd51ae711516de1418f45e1efd3540c0a7da38b
ababinili <129664145+ababinili@users.noreply.github.com>
2023-04-08 00:30:51 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+const input = document.querySelector("input")
+
+
+function funcerti(){
+    input.value += "1"
+}
+function funcori(){
+    input.value += "2"
+}
+function funcsami(){
+    input.value += "3"
+}
+function funcplus(){
+    input.value += "+"
+}
+function funcotxi(){
+    input.value += "4"
+}
+function funcxuti(){
+    input.value += "5"
+}
+function funceqvsi(){
+    input.value += "6"
+}
+function funcminus(){
+    input.value +="-"
+}
+function funcshvidi(){
+    input.value += "7"
+}
+function funcrva(){
+    input.value += "8"
+}
+function funccxra(){
+    input.value += "9"
+}
+function funcmult(){
+    input.value += "*"
+}
+function funcc(){
+    input.value = ""
+}
+function  funczero(){
+    input.value += "0"
+}
+function funcdiv(){
+    input.value += "/"
+}
+function funcequal(){
+    input.value = eval(input.value)
+}
